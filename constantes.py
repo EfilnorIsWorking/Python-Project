9a4758c04d0af5f8154898ab6fcca2379b68bb42
EfilnorIsWorking <145761924+EfilnorIsWorking@users.noreply.github.com>
2023-10-25 15:23:14 +0200

    modifs

--- constantes.py
+++ constantes.py
@@ -1,6 +1,7 @@
-nbBob = 100
-D = 100
-energy_max = 200
-energy_food = 100
-quantity_food = 200
-Mutv = 0.1
+nbBob = 10 #nb de Bobs
+D = 100 #Nombre de ticks par jour [pas obliger de le rendre modifiable]
+energy_max = 200 #Energie maximum d'un Bob
+energy_food = 100 #Energie donnée par une portion de nourriture
+quantity_food = 20 #Quantité de nourriture qui apparaît sur la map
+Mutv = 0.1 #Coefficient de mutation
+taille_grille = 10 #Taille de la grille
