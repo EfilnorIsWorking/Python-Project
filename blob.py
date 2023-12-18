6008bb56b74285989e21e29ecd7a081ffe4f9ebd
EfilnorIsWorking <145761924+EfilnorIsWorking@users.noreply.github.com>
2023-12-18 14:32:58 +0100

    Update blob.py
    création des variables de perception, de masse et de vitesse

--- blob.py
+++ blob.py
@@ -1,10 +1,59 @@
-from constantes import *
+import os
+import random
+import math
+import pygame
+from PIL import Image
+import array
+import sys
+
 
 class Blob :
-    def __init__(self):
+    MUTATION_RAT = 0.1
+    def __init__(self, key, parent_name1, parent_name2):
         self.energy = 100
+        self.energy_max = 200
+
         self.alive = True
-        self.vitesse = 1
+        self.x = random.randint(0, taille_grille-1)
+        self.y = random.randint(0, taille_grille-1)
+
+        self.key = key
+        self.parent_key1 = parent_name1.key
+        self.parent_key2 = parent_name2.key
+
+        if (self.parent_key1 != 0):
+            self.vitesse = random(parent_name1.vitesse - MUTATION_RAT, parent_name1.vitesse + MUTATION_RAT)
+            self.mass = random(parent_name1.mass - MUTATION_RAT, parent_name1.mass + MUTATION_RAT)
+            self.perception += random.randint(-1, 1)
+        else :
+            self.vitesse = 1
+            self.mass = 1
+            self.perception = 0
+
+    def where_to_go(self)
+        #modifier de manière à observer ce qu'il y a dans les cases
+        move(self, #direction)
+
+
+    def move(self, direction):
+        if direction == 0 :     #DOWN
+            self.y += self.vitesse
+            self.energy -= self.vitesse*self.vitesse
+
+        elif direction == 1 :   #UP
+            self.y -= self.vitesse
+            self.energy -= self.vitesse*self.vitesse*self.mass
+
+        elif direction == 2 :   #RIGHT
+            self.x += self.vitesse
+            self.energy -= self.vitesse*self.vitesse*self.mass
+
+        elif direction == 3:                   #LEFT
+            self.x -= self.vitesse
+            self.energy -= self.vitesse*self.vitesse*self.mass
+        else :
+            self.energy -= 0.5
+        #FAIRE BUFFER DIRECTIONNEL
 
     def energyGain (self, v):
         if (v<energy_max):
@@ -17,7 +66,16 @@ class Blob :
 
 def birth_parth(bobMom):
     if bobMom.energy == energy_max :
-       bob = blob()
+       bob = Blob()
        bob.energy = 50
        bobMom.energy -= 150
        return bob
+
+def encounter_bobs(bob1, bob2):
+    if (bob1.mass/bob2.mass < 2/3)
+        bob1.alive = False
+        bob2.energy -= (1/2)*(bob1.mass/bob2.mass)*bob1.energy + (1/2)*bob1.energy
+    if (bob2.mass/bob1.mass < 2/3)
+        bob2.alive = False
+        bob1.energy -= (1/2)*(bob2.mass/bob1.mass)*bob2.energy + (1/2)*bob2.energy
+
